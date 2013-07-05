fdd4585fd301fae4fadec208e940215614462895
Justin Aquadro <jaquadro@gmail.com>
2013-07-04 22:58:24 -0400

    Fix resource paths

--- ExtraButtons/src/com/jaquadro/minecraft/extrabuttons/CommonProxy.java
+++ ExtraButtons/src/com/jaquadro/minecraft/extrabuttons/CommonProxy.java
@@ -2,8 +2,8 @@ package com.jaquadro.minecraft.extrabuttons;
 
 public class CommonProxy
 {
-    public static String ITEMS_PNG = ExtraButtons.RESOURCE_PATH + "/items.png";
-    public static String BLOCK_PNG = ExtraButtons.RESOURCE_PATH + "/block.png";
+    public static String ITEMS_PNG = ExtraButtons.RESOURCE_PATH + "items.png";
+    public static String BLOCK_PNG = ExtraButtons.RESOURCE_PATH + "block.png";
 
     public void registerRenderers () {
 
