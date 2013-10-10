a4d2a843e9f9f9f2f11b2d1845e38a08b4611db8
Justin Aquadro <jaquadro@gmail.com>
2013-10-09 22:34:13 -0400

    Update version

--- ExtraButtons/src/com/jaquadro/minecraft/extrabuttons/ExtraButtons.java
+++ ExtraButtons/src/com/jaquadro/minecraft/extrabuttons/ExtraButtons.java
@@ -28,7 +28,7 @@ public class ExtraButtons
 {
     public static final String MOD_ID = "ExtraButtons";
     static final String MOD_NAME = "ExtraButtons";
-    static final String MOD_VERSION = "1.6.4.0";
+    static final String MOD_VERSION = "1.6.4.1";
     static final String SOURCE_PATH = "com.jaquadro.minecraft.extrabuttons.";
 
     private static int capacitiveBlockId;
