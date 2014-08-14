9136a29595cd9f3f8fddb6c469c9403f827961b8
Justin Aquadro <jaquadro@gmail.com>
2014-08-14 00:05:04 -0400

    Version updates

--- GardenContainers/src/com/jaquadro/minecraft/gardencontainers/GardenContainers.java
+++ GardenContainers/src/com/jaquadro/minecraft/gardencontainers/GardenContainers.java
@@ -24,7 +24,7 @@ public class GardenContainers
 {
     public static final String MOD_ID = "GardenContainers";
     public static final String MOD_NAME = "Garden Containers";
-    public static final String MOD_VERSION = "1.1.1";
+    public static final String MOD_VERSION = "1.1.2";
     static final String SOURCE_PATH = "com.jaquadro.minecraft.gardencontainers.";
 
     public static final ModBlocks blocks = new ModBlocks();
