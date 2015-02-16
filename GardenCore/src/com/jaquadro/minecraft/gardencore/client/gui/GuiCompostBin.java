9666f1f9fe296233e623065dc9ea6a13563d87c7
Justin Aquadro <jaquadro@gmail.com>
2015-02-15 20:36:28 -0500

    Highlight compatible items in GUIs

--- GardenCore/src/com/jaquadro/minecraft/gardencore/client/gui/GuiCompostBin.java
+++ GardenCore/src/com/jaquadro/minecraft/gardencore/client/gui/GuiCompostBin.java
@@ -7,6 +7,7 @@ import net.minecraft.client.gui.inventory.GuiContainer;
 import net.minecraft.client.resources.I18n;
 import net.minecraft.entity.player.InventoryPlayer;
 import net.minecraft.inventory.Container;
+import net.minecraft.inventory.Slot;
 import net.minecraft.util.ResourceLocation;
 import org.lwjgl.opengl.GL11;
 
@@ -15,16 +16,37 @@ public class GuiCompostBin extends GuiContainer
     private static final ResourceLocation compostBinGuiTextures = new ResourceLocation("gardencore", "textures/gui/compostBin.png");
     private TileEntityCompostBin tileCompost;
 
+    private Slot hoveredSlot;
+
     public GuiCompostBin (InventoryPlayer inventory, TileEntityCompostBin tileEntity) {
         super(new ContainerCompostBin(inventory, tileEntity));
         tileCompost = tileEntity;
     }
 
-    protected void drawGuiContainerForegroundLayer(int p_146979_1_, int p_146979_2_)
-    {
+    @Override
+    public void drawScreen (int mouseX, int mouseY, float dt) {
+        hoveredSlot = null;
+        for (int i = 0, n = inventorySlots.inventorySlots.size(); i < n; i++) {
+            Slot slot = inventorySlots.getSlot(i);
+            if (func_146978_c(slot.xDisplayPosition, slot.yDisplayPosition, 16, 16, mouseX, mouseY))
+                hoveredSlot = slot;
+        }
+
+        super.drawScreen(mouseX, mouseY, dt);
+    }
+
+    @Override
+    protected void drawGuiContainerForegroundLayer(int p_146979_1_, int p_146979_2_) {
+        GL11.glPushAttrib(GL11.GL_ALL_ATTRIB_BITS);
+
         String name = this.tileCompost.hasCustomInventoryName() ? this.tileCompost.getInventoryName() : I18n.format(this.tileCompost.getInventoryName(), new Object[0]);
         this.fontRendererObj.drawString(name, this.xSize / 2 - this.fontRendererObj.getStringWidth(name) / 2, 6, 4210752);
         this.fontRendererObj.drawString(I18n.format("container.inventory", new Object[0]), 8, this.ySize - 96 + 2, 4210752);
+
+        for (int i = 0, n = inventorySlots.inventorySlots.size(); i < n; i++)
+            drawSlotHighlight(inventorySlots.getSlot(i));
+
+        GL11.glPopAttrib();
     }
 
     @Override
@@ -39,5 +61,22 @@ public class GuiCompostBin extends GuiContainer
             int timeRemaining = tileCompost.getDecomposeTimeRemainingScaled(24);
             drawTexturedModalRect(halfW + 89, halfH + 34, 176, 0, 24 - timeRemaining + 1, 16);
         }
+    }
+
+    protected void drawSlotHighlight (Slot slot) {
+        if (hoveredSlot == null || isInPlayerInventory(hoveredSlot) || slot == hoveredSlot)
+            return;
+
+        if (mc.thePlayer.inventory.getItemStack() == null) {
+            if (slot != null && slot.getHasStack() && hoveredSlot.isItemValid(slot.getStack())) {
+                zLevel += 100;
+                drawGradientRect(slot.xDisplayPosition, slot.yDisplayPosition, slot.xDisplayPosition + 16, slot.yDisplayPosition + 16, 0x80ffffff, 0x80ffffff);
+                zLevel -= 100;
+            }
+        }
+    }
+
+    private boolean isInPlayerInventory (Slot slot) {
+        return slot.inventory == mc.thePlayer.inventory;
     }
 }
--- GardenCore/src/com/jaquadro/minecraft/gardencore/client/gui/GuiGardenLayout.java
+++ GardenCore/src/com/jaquadro/minecraft/gardencore/client/gui/GuiGardenLayout.java
@@ -1,12 +1,11 @@
 package com.jaquadro.minecraft.gardencore.client.gui;
 
-import com.jaquadro.minecraft.gardencore.GardenCore;
-import com.jaquadro.minecraft.gardencore.block.BlockGarden;
 import com.jaquadro.minecraft.gardencore.block.tile.TileEntityGarden;
 import com.jaquadro.minecraft.gardencore.inventory.ContainerGarden;
 import net.minecraft.client.gui.inventory.GuiContainer;
 import net.minecraft.client.resources.I18n;
 import net.minecraft.entity.player.InventoryPlayer;
+import net.minecraft.inventory.Slot;
 import net.minecraft.util.ResourceLocation;
 import org.lwjgl.opengl.GL11;
 
@@ -14,6 +13,8 @@ public class GuiGardenLayout extends GuiContainer
 {
     private static final ResourceLocation guiTextures = new ResourceLocation("gardencore", "textures/gui/gardenLayout.png");
     private TileEntityGarden tileGarden;
+
+    private Slot hoveredSlot;
 
     private static final int slotBaseX = 44;
     private static final int slotBaseY = 18;
@@ -29,10 +30,29 @@ public class GuiGardenLayout extends GuiContainer
     }
 
     @Override
+    public void drawScreen (int mouseX, int mouseY, float dt) {
+        hoveredSlot = null;
+        for (int i = 0, n = inventorySlots.inventorySlots.size(); i < n; i++) {
+            Slot slot = inventorySlots.getSlot(i);
+            if (func_146978_c(slot.xDisplayPosition, slot.yDisplayPosition, 16, 16, mouseX, mouseY))
+                hoveredSlot = slot;
+        }
+
+        super.drawScreen(mouseX, mouseY, dt);
+    }
+
+    @Override
     protected void drawGuiContainerForegroundLayer (int p_146979_1_, int p_146979_2_) {
+        GL11.glPushAttrib(GL11.GL_ALL_ATTRIB_BITS);
+
         String name = tileGarden.hasCustomInventoryName() ? tileGarden.getInventoryName() : I18n.format(tileGarden.getInventoryName(), new Object[0]);
         fontRendererObj.drawString(name, 8, 6, 4210752);
         fontRendererObj.drawString(I18n.format("container.inventory", new Object[0]), 8, ySize - 96 + 2, 4210752);
+
+        for (int i = 0, n = inventorySlots.inventorySlots.size(); i < n; i++)
+            drawSlotHighlight(inventorySlots.getSlot(i));
+
+        GL11.glPopAttrib();
     }
 
     @Override
@@ -60,5 +80,22 @@ public class GuiGardenLayout extends GuiContainer
             drawTexturedModalRect(guiX + slotBaseX, guiY + slotBaseY + 18 * 4, smDisabledX, smDisabledY, 16, 16);
         if (!tileGarden.isAttachedNeighbor(tileGarden.xCoord - 1, tileGarden.yCoord, tileGarden.zCoord))
             drawTexturedModalRect(guiX + slotBaseX, guiY + slotBaseY + 18 * 2, smDisabledX, smDisabledY, 16, 16);
+    }
+
+    protected void drawSlotHighlight (Slot slot) {
+        if (hoveredSlot == null || isInPlayerInventory(hoveredSlot) || slot == hoveredSlot)
+            return;
+
+        if (mc.thePlayer.inventory.getItemStack() == null) {
+            if (slot != null && slot.getHasStack() && hoveredSlot.isItemValid(slot.getStack())) {
+                zLevel += 100;
+                drawGradientRect(slot.xDisplayPosition, slot.yDisplayPosition, slot.xDisplayPosition + 16, slot.yDisplayPosition + 16, 0x80ffffff, 0x80ffffff);
+                zLevel -= 100;
+            }
+        }
+    }
+
+    private boolean isInPlayerInventory (Slot slot) {
+        return slot.inventory == mc.thePlayer.inventory;
     }
 }
