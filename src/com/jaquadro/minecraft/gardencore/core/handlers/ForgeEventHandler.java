56a7f166a09ce65325b3f42bb58ae65061021eb1
Justin Aquadro <jaquadro@gmail.com>
2015-09-07 14:27:16 -0400

    Disable interaction between Botania seed bag and garden blocks

--- src/com/jaquadro/minecraft/gardencore/core/handlers/ForgeEventHandler.java
+++ src/com/jaquadro/minecraft/gardencore/core/handlers/ForgeEventHandler.java
@@ -1,13 +1,14 @@
 package com.jaquadro.minecraft.gardencore.core.handlers;
 
 import com.jaquadro.minecraft.gardencore.api.IPlantProxy;
+import com.jaquadro.minecraft.gardencore.api.block.IGardenBlock;
 import com.jaquadro.minecraft.gardencore.block.BlockGarden;
-import com.jaquadro.minecraft.gardencore.block.BlockGardenProxy;
-import com.jaquadro.minecraft.gardencore.block.BlockGardenSoil;
 import com.jaquadro.minecraft.gardencore.block.BlockSmallFire;
 import cpw.mods.fml.common.eventhandler.Event;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
+import cpw.mods.fml.common.registry.GameData;
 import net.minecraft.block.Block;
+import net.minecraft.item.ItemStack;
 import net.minecraftforge.event.entity.player.BonemealEvent;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import net.minecraftforge.event.entity.player.UseHoeEvent;
@@ -25,6 +26,17 @@ public class ForgeEventHandler
 
     @SubscribeEvent
     public void playerInteracts (PlayerInteractEvent event) {
+        if (event.action == PlayerInteractEvent.Action.RIGHT_CLICK_BLOCK) {
+            ItemStack stack = event.entityPlayer.inventory.getCurrentItem();
+            if (stack != null && stack.getItem() == GameData.getItemRegistry().getObject("Botania:flowerBag")) {
+                Block block = event.world.getBlock(event.x, event.y, event.z);
+                if (block instanceof IGardenBlock) {
+                    event.useItem = Event.Result.DENY;
+                    return;
+                }
+            }
+        }
+
         if (event.action == PlayerInteractEvent.Action.LEFT_CLICK_BLOCK) {
             if (event.entityPlayer.capabilities.isCreativeMode) {
                 if (BlockSmallFire.extinguishSmallFire(event.world, null, event.x, event.y, event.z, event.face)) {
